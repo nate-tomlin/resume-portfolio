bb243fb117b983d3e8df6f093c9d7449f6810d22
nate-tomlin <48532338+nate-tomlin@users.noreply.github.com>
2020-03-26 18:10:33 -0400

    Finished Mobile Responsiveness

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,28 +2,35 @@
 
 //Select DOM Items
 const menuBTN = document.querySelector('.menu-btn');
+const menuBTN2 = document.querySelector('.menu-btn-2x');
 const menuBTNClose = document.querySelector('.menu-btn-close');
-const links = document.querySelector('.links');
+const links = document.querySelector('.links-mobile');
 
 //Set Initial State of Menu
 let showMenu = false;
 
 //Add EventListeners to DOM Items
 menuBTN.addEventListener('click', toggleMenu);
+menuBTN2.addEventListener('click', toggleMenu);
 menuBTNClose.addEventListener('click', toggleMenu);
 links.addEventListener('click', toggleMenu);
 
 //Function That Moves in Menu Items
 function toggleMenu() {
     if (!showMenu) {
-        document.getElementById('menu').style.left = '0';
-        document.getElementById('menu-background').style.left = '0';
+        if (window.innerWidth <= 1120) {
+            document.getElementById('menu-mobile').style.left = '0';
+            document.getElementById('menu-background').style.left = '0';
 
-        showMenu = true;
+            showMenu = true;
+        }
     } else {
-        document.getElementById('menu').style.left = '-300px';
-        document.getElementById('menu-background').style.left = '100%';
+        if (window.innerWidth <= 1120) {
+            document.getElementById('menu-mobile').style.left = '-300px';
+            document.getElementById('menu-background').style.left = '100%';
 
-        showMenu = false;
+            showMenu = false;
+        }
     }
+
 }
