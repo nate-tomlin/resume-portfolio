2b2cede5d4a32845e514cbdb540be1d7fbe82377
nate-tomlin <48532338+nate-tomlin@users.noreply.github.com>
2020-03-31 08:29:01 -0400

    Fixed Error in Contact JS

--- js/contact.js
+++ js/contact.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
         //Validations
         //First Name Validation
         if (firstName.length >= 2) {
-            statusFN.append('<small style="color: darkgreen;>First Name is Valid</small>');
+            statusFN.append('<small style="color: darkgreen;">First Name is Valid</small>');
         } else {
             event.preventDefault(); //Prevents Sending Form
             statusFN.append('<small style="color: maroon;">First Name is Not Valid</small>');
@@ -36,7 +36,7 @@ $(document).ready(function () {
 
         //Last Name Validation
         if (lastName.length >= 2) {
-            statusLN.append('<small style="color: darkgreen;>Last Name is Valid</small>');
+            statusLN.append('<small style="color: darkgreen;">Last Name is Valid</small>');
         } else {
             event.preventDefault(); //Prevents Sending Form
             statusLN.append('<small style="color: maroon;">Last Name is Not Valid</small>');
@@ -44,7 +44,7 @@ $(document).ready(function () {
 
         //Email Validation
         if (email.length > 5 && email.includes('@') && email.includes('.')) {
-            statusEmail.append('<small style="color: darkgreen;>Email is Valid</small>');
+            statusEmail.append('<small style="color: darkgreen;">Email is Valid</small>');
         } else {
             event.preventDefault(); //Prevents Sending Form
             statusEmail.append('<small style="color: maroon;">Email is Not Valid</small>');
@@ -52,7 +52,7 @@ $(document).ready(function () {
 
         //Subject Validation
         if (subject.length >= 2) {
-            statusSubject.append('<small style="color: darkgreen;>Subject is Valid</small>');
+            statusSubject.append('<small style="color: darkgreen;">Subject is Valid</small>');
         } else {
             event.preventDefault(); //Prevents Sending Form
             statusSubject.append('<small style="color: maroon;">Subject is Not Valid</small>');
@@ -60,7 +60,7 @@ $(document).ready(function () {
 
         //Message Validation
         if (message.length >= 2) {
-            statusMessage.append('<small style="color: darkgreen;>Message is Valid</small>');
+            statusMessage.append('<small style="color: darkgreen;">Message is Valid</small>');
         } else {
             event.preventDefault(); //Prevents Sending Form
             statusMessage.append('<small style="color: maroon;">Message is Not Valid</small>');
